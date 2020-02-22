e8e19428e7bc90ecd4d8f70214b78b24927a91eb
Gyula Kiri <38438001+duszmox@users.noreply.github.com>
2020-02-22 10:59:23 +0100

    commit2
    Some basics

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php 
 ?>
 <html>
-
-
+    <head>
+    <link rel="stylesheet" href="main_style.css">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+    
+</body>
 
 </html>
