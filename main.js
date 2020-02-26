5c8eaabca26e3148b4819ba530c61d1cefa320e3
Gyula Kiri <38438001+duszmox@users.noreply.github.com>
2020-02-26 19:39:53 +0100

    Day End commit
    New texts, home page content, statistic page, onyl lower case code, ect.

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 function hideForm() {
     document.getElementById("code-form").style.display="none";
 };
+function voteInfo() {
+    Swal.fire(
+        'Válassz osztályt!',
+        '',
+        'info'
+      )
+}
+function notRealCode() {
+    Swal.fire({
+        title: 'Nem létező kódot próbálsz használni!',
+        icon: 'error',
+        html:
+          'Nézd meg, nem-e ütötted el, vagy használj másik kódot!',
+        focusConfirm: false,
+        confirmButtonText: 'Vissza'
+    }).then((result) => {
+      if (result.value) {
+        window.location.href = "vote.php";
+      }
+    })
+       
+}
+
+function visibleForm() {
+    document.getElementById("code-form").style.display="";
+};
--- navbar.php
+++ navbar.php
@@ -1,15 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand nav-title" href="#">LUDAVATO</a>
+  <a class="navbar-brand nav-title" href="http://duszmo.f.fazekas.hu/lavato/">LUDAVATO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="http://duszmo.f.fazekas.hu/lavato/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="vote.php" >Szavazás<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="status.php" >Helyezések<span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
