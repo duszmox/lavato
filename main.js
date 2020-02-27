f38e241f9f61724ee94f696150a22a88b03148a3
horvathandrasmate <33268751+horvathandrasmate@users.noreply.github.com>
2020-02-27 09:32:11 +0100

    Upload vote 0.1

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ function confirmVote(i, cond=false) {
         'Sikeres szavazás!',
         'Szavazatod elmenésre került!',
         'success'
-      )
-
+      );
+document.location.href = ""
 
     }
   })
