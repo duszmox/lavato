0126c1553b4bb1707c307e0d4bf1f8c7f2d96de7
Gyula Kiri <38438001+duszmox@users.noreply.github.com>
2020-02-28 20:09:37 +0100

    Make every source file local

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="assets/fonts/leaves/stylesheet.css">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/js/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Lavató</title>
     </head>
 <body class="bg">
