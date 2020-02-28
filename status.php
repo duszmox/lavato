7a3f29a152eef64344e3898495a7abd2b1aa9afc
horvathandrasmate <33268751+horvathandrasmate@users.noreply.github.com>
2020-02-28 22:57:32 +0100

    Login system

--- status.php
+++ status.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+require_once ("snippets.php");
+if(!user_logged_in()){
+    header("Location: /lavato/");
+}
+?>
 <head>
     <title>Lavato-Helyezések</title>
-    <link rel="stylesheet" href="main_style.css">
-    <link rel="stylesheet" href="assets/fonts/leaves/stylesheet.css">
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/js/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="main.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery-3.4.1.slim.min.js"
-            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-            crossorigin="anonymous"></script>
-    <script src="assets/js/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-    <script src="assets/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-            crossorigin="anonymous"></script>
-    <script src="assets/js/core.js"></script>
-    <script src="assets/js/charts.js"></script>
-    <script src="assets/js/animated.js"></script>
+
     <script>
         <?php
         require_once("snippets.php");
