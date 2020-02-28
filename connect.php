48c2dd6bb2bbdfcfa0234fe62766ce231cbbd587
horvathandrasmate <33268751+horvathandrasmate@users.noreply.github.com>
2020-02-28 11:28:25 +0100

    Revert "Revert "Unfinished commit , Andras""
    This reverts commit 2ad189cbc83a39be1cdacaa53523923151ac6b5b.

--- connect.php
+++ connect.php
@@ -1,7 +1,21 @@
-
 <?php
-$conn = mysqli_connect("localhost", "duszmo", "ThanosPapa9?", "duszmo");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "lavato";
+
+// Create connection
+
+    global $conn;
+    $conn = new mysqli($servername, $username, $password, $db);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        //echo("<script>alert('We are gooing.')</script>");
+    }
+
+
+
 ?>
