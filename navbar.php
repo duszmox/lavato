7d9ec555131aacf74635a7af404cf89e9f120d8f
Gyula Kiri <duszmo@f.fazekas.hu>
2020-02-29 23:43:13 +0100

    Login import, register, hashgen, log,

--- navbar.php
+++ navbar.php
@@ -2,22 +2,18 @@
 <link rel="stylesheet" href="assets/fonts/leaves/stylesheet.css">
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="assets/js/bootstrap.min.css"
+<link rel="stylesheet" href="assets/js/bootstrap.min.css">
 <script src="main.js"></script>
 <script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery-3.4.1.slim.min.js"
-        crossorigin="anonymous"></script>
-<script src="assets/js/popper.min.js"
-        crossorigin="anonymous"></script>
-<script src="assets/js/bootstrap.min.js"
-        crossorigin="anonymous"></script>
+<script src="assets/js/jquery-3.4.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="assets/js/popper.min.js" crossorigin="anonymous"></script>
+<script src="assets/js/bootstrap.min.js" crossorigin="anonymous"></script>
 <script src="assets/js/core.js"></script>
 <script src="assets/js/charts.js"></script>
 <script src="assets/js/animated.js"></script>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand nav-title" href="/lavato/">LUDAVATO</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
@@ -29,23 +25,23 @@
                 <a class="nav-link" href="vote.php">Szavazás<span class="sr-only">(current)</span></a>
             </li>
             <?php if (user_logged_in()) {
-                ?>
+            ?>
                 <li class="nav-item active">
                     <a class="nav-link" href="status.php">Helyezések<span class="sr-only">(current)</span></a>
                 </li>
 
-                <?php
+            <?php
             }
             if (is_admin()) {
-                ?>
+            ?>
                 <li class="nav-item active">
                     <a class="nav-link" href="status.php">Adatok<span class="sr-only">(current)</span></a>
                 </li>
 
-                <?php
+            <?php
             }
-            if(user_logged_in()){
-                ?>
+            if (user_logged_in()) {
+            ?>
                 <li class="nav-item active">
                     <a class="nav-link" href="logout.php">Logout<span class="sr-only">(current)</span></a>
                 </li>
@@ -56,5 +52,3 @@
         </ul>
     </div>
 </nav>
-
-
