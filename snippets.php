4a57f0695874cd322bd845d8c72ead340ac2ebb0
Gyula Kiri <duszmo@f.fazekas.hu>
2020-02-29 23:48:38 +0100

    Log, register, login, admin, hashgen

--- snippets.php
+++ snippets.php
@@ -1,12 +1,13 @@
 <?php
 require_once("connect.php");
 
-function html_escape($html_escape){
+function html_escape($html_escape)
+{
     $html_escape = htmlspecialchars($html_escape, ENT_QUOTES | ENT_HTML5, 'UTF-8');
     return $html_escape;
 }
 
-function swal_error($title,$text)
+function swal_error($title, $text)
 {
     echo "<script>
     Swal.fire({
@@ -29,8 +30,7 @@ function verify_hash($hash)
     global $conn;
     $sql = "SELECT * FROM lavato_keys WHERE hash = '$hash'";
     $row = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($row) == 1)
-    {
+    if (mysqli_num_rows($row) == 1) {
         log_action("hash_verification", "$hash _verified");
         return true;
     } else {
@@ -57,11 +57,10 @@ function disable_hash($hash)
 function log_action($action, $value)
 {
     global $conn;
-    $sql = "INSERT INTO lavato_log (action, value) VALUES ('$action', '$value')";
+    $date = date("Y-m-d H:i:s");
+    $sql = "INSERT INTO lavato_log (action, value, date) VALUES ('$action', '$value', '$date')";
 
     return mysqli_query($conn, $sql);
-
-
 }
 
 function upload_vote($class, $hash)
@@ -71,13 +70,14 @@ function upload_vote($class, $hash)
     log_action("class_vote", "$class");
     return mysqli_query($conn, $sql);
 }
-function get_class_votes(){
+function get_class_votes()
+{
     global $conn;
     $sql = "SELECT * FROM lavato_keys WHERE hasBeenActivated=1";
-    $raw = mysqli_query($conn,$sql);
-    $class_data = array("A" => 0, "B" => 0,"C" => 0,"D" => 0);
-    while($res = mysqli_fetch_array($raw)){
-        switch ($res["class"]){
+    $raw = mysqli_query($conn, $sql);
+    $class_data = array("A" => 0, "B" => 0, "C" => 0, "D" => 0);
+    while ($res = mysqli_fetch_array($raw)) {
+        switch ($res["class"]) {
             case 'A':
                 $class_data['A'] += 1;
                 break;
@@ -91,7 +91,6 @@ function get_class_votes(){
                 $class_data['D'] += 1;
                 break;
         }
-
     }
 
 
@@ -102,51 +101,55 @@ function get_class_votes(){
 }
 
 
-function create_random_data($max, $hasBeenActivated){
+
+function create_random_data($max, $hasBeenActivated, $classes)
+{
     global $conn;
-    $classes = array("A","B","C","D");
-    for($i =0;$i<$max;$i++){
-        $class = rand(0,3);
-        $hash = bin2hex(random_bytes(32));
-        $sql = "INSERT INTO lavato_keys (hash, hasBeenActivated,class) VALUES ('$hash', '$hasBeenActivated' ,'$classes[$class]')";
+    for ($i = 0; $i < $max; $i++) {
+        $token = openssl_random_pseudo_bytes(32);
+        $token = bin2hex($token);
+        $sql = "INSERT INTO lavato_keys (hash, hasBeenActivated, class) VALUES ('$token', '$hasBeenActivated' ,'$classes')";
         mysqli_query($conn, $sql);
         //todo ellenorize, hogy van-e mar ilyen hash
+        
     }
+    return true;
 }
 
-function hasBeenActivated($hash) {
+function hasBeenActivated($hash)
+{
     global $conn;
     $sql = "SELECT * FROM lavato_keys WHERE hash='$hash' AND hasBeenActivated=0";
     $row = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($row) == 1)
-    {
+    if (mysqli_num_rows($row) == 1) {
         log_action("hash_activated", "$hash _activated");
         return true;
     } else {
         return false;
     }
 }
-function validate_user($username, $password){
+function validate_user($username, $password)
+{
     global $conn;
     $sql = "SELECT * FROM lavato_users WHERE username='$username'";
     $row = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($row) == 1)
-    {
+    if (mysqli_num_rows($row) == 1) {
         $result = mysqli_fetch_assoc($row);
-        if($result['password'] == hash("sha256", $password)){
+        if ($result['password'] == hash("sha256", $password)) {
 
             return true;
-
         }
     } else {
         return false;
     }
 }
-function get_userdata($username){
+function get_userdata($username)
+{
     global $conn;
     return mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM lavato_users WHERE username='$username'"));
 }
-function login_user($username){
+function login_user($username)
+{
     log_action("login", $username);
     $userdata = get_userdata($username);
 
@@ -156,31 +159,57 @@ function login_user($username){
 
     return true;
 }
-function logout_user($username){
+function logout_user($username)
+{
     log_action("logout", $username);
 
     $_SESSION['username'] = "";
     $_SESSION['admin'] = "";
     $_SESSION['logged_in'] = 0;
 }
-function user_logged_in(){
+function user_logged_in()
+{
     return $_SESSION['logged_in'];
 }
-function is_admin(){
+function is_admin()
+{
     return $_SESSION['admin'];
 }
-function register_user($username, $password){
-    if(!can_register()){return false;}
+function register_user($username, $password)
+{
+   
     global $conn;
-    if(!empty(get_userdata($username))){
+    if (!empty(get_userdata($username))) {
         echo "Már foglalt felhaszálónév";
         return false;
     }
-    $sql = "INSERT INTO lavato_users (username, password) VALUES ('$username', '".hash("sha256", $password)."')";
-    echo $sql;
+    $sql = "INSERT INTO lavato_users (username, password) VALUES ('$username', '" . hash("sha256", $password) . "')";
     log_action("register_user", $username);
     return mysqli_query($conn, $sql);
 }
 
-
+function get_log()
+{
+    global $conn;
+    if (!$conn) {
+        die('Could not connect: ' . $conn->connect_error());
+    }
+    $sql = 'SELECT action, value, date, id FROM lavato_log';
+    $retval = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 ?>
+        <tr>
+            <th scope="row"><?php echo "{$row['id']}" ?></th>
+            <td><?php echo "{$row['action']}" ?> </td>
+            <td> <?php echo "{$row['value']}" ?></td>
+            <td> <?php echo "{$row['date']}" ?></td>
+        </tr>
+<?php
+    }
+}
+
+function goBack() {
+    ?>
+    <script> window.history.back()</script>
+    <?php
+}
--- status.php
+++ status.php
@@ -1,39 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>Lavato-Helyezések</title>
+    <title>Lavato - Helyezések</title>
     <link rel="stylesheet" href="main_style.css">
     <link rel="stylesheet" href="assets/fonts/leaves/stylesheet.css">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/js/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/js/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="main.js"></script>
     <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery-3.4.1.slim.min.js"
-            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-            crossorigin="anonymous"></script>
-    <script src="assets/js/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-    <script src="assets/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-            crossorigin="anonymous"></script>
+    <script src="assets/js/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="assets/js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="assets/js/core.js"></script>
     <script src="assets/js/charts.js"></script>
     <script src="assets/js/animated.js"></script>
     <script>
         <?php
         require_once("snippets.php");
+        if(!user_logged_in()) {
+            goBack();
+        }
         $data = get_class_votes();
         $data_pretty = get_class_votes();
 
-        $all = $data['A']+$data['B']+$data['C']+$data['D'];
-        foreach($data as $key => $value){
-            $data[$key] = $value/$all *100;
+        $all = $data['A'] + $data['B'] + $data['C'] + $data['D'];
+        foreach ($data as $key => $value) {
+            $data[$key] = $value / $all * 100;
         }
         ?>
-        window.onload = function () {
+        window.onload = function() {
 
             var chart = new CanvasJS.Chart("chartContainer", {
                 animationEnabled: true,
@@ -42,14 +39,25 @@
                 },
                 data: [{
                     type: "pie",
-                    startAngle: 240, 
+                    startAngle: 240,
                     yValueFormatString: "0.00\"%\"",
                     indexLabel: "{label} {y}",
-                    dataPoints: [
-                        {y: <?php echo $data['A']?>, label: "A - <?php echo $data_pretty['A']?> szavazat,"},
-                        {y: <?php echo $data['B']?>, label: "B - <?php echo $data_pretty['B']?> szavazat,"},
-                        {y: <?php echo $data['C']?>, label: "C - <?php echo $data_pretty['C']?> szavazat,"},
-                        {y: <?php echo $data['D']?>, label: "D - <?php echo $data_pretty['D']?> szavazat,"}
+                    dataPoints: [{
+                            y: <?php echo $data['A'] ?>,
+                            label: "A - <?php echo $data_pretty['A'] ?> szavazat,"
+                        },
+                        {
+                            y: <?php echo $data['B'] ?>,
+                            label: "B - <?php echo $data_pretty['B'] ?> szavazat,"
+                        },
+                        {
+                            y: <?php echo $data['C'] ?>,
+                            label: "C - <?php echo $data_pretty['C'] ?> szavazat,"
+                        },
+                        {
+                            y: <?php echo $data['D'] ?>,
+                            label: "D - <?php echo $data_pretty['D'] ?> szavazat,"
+                        }
                     ]
                 }]
             });
@@ -57,20 +65,24 @@
 
         }
     </script>
+    <script src="assets/js/canvasjs.min.js"></script>
+
 </head>
 
 <body>
 
-<?php
+    <?php
 
-require("navbar.php");
-//create_random_data(50, 1);
+    require("navbar.php");
+    //create_random_data(50, 1);
 
 
-?>
+    ?>
+    <div class="main-container container" style="padding-bottom: 3vw">
+        <div id="chartContainer"></div>
+    </div>
 
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="assets/js/canvasjs.min.js"></script>
 
 </body>
+
 </html>
