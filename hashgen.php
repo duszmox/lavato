922febd8f8cc1466cd1dac63f862e8b7db11eb4b
Gyula Kiri <duszmo@f.fazekas.hu>
2020-02-29 23:48:38 +0100

    Log, register, login, admin, hashgen

---
+++ hashgen.php
@@ -0,0 +1,41 @@
+<?php
+require_once("snippets.php");
+require_once("navbar.php");
+if (!is_admin()) {
+    goBack();
+}
+else if (isset($_POST['submit']) && $_POST['submit'] == "Generálás") {
+    if (isset($_POST['max-character'])) {
+        $number = html_escape($_POST['max-character']);
+        if (create_random_data($number, '0', '0')) {
+            ?>
+            <script>successfulCodeGen(<?php echo $number?>)</script>
+            <?php
+        }
+    } 
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lavató - Hash Generátor</title>
+</head>
+
+<body>
+    <div class="main-container container">
+        <h1 class="hash-h1">Hash Generátor</h1>
+        <hr class="hash-hr">
+        <div class="form-group login-form">
+            <form action="hashgen.php" method="POST">
+                <label for="max-character"><strong>Generálandó kódot száma:</strong></label>
+                <input type="number" class="form-control" placeholder="Generálandó kódok száma" name="max-character" id="max-character">
+                <input type="submit" class="btn hash-submit-button btn-primary" name="submit" value="Generálás">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ log.php
@@ -0,0 +1,42 @@
+<?php
+require_once("snippets.php");
+require_once("navbar.php");
+if (!is_admin()) {
+    goBack();
+
+};
+?>
+
+
+<head>
+    <title>Lavató - Eseménylista</title>
+    <link href="assets/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <script src="assets/js/jquery-3.3.1.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+
+</head>
+<script>
+    $(document).ready(function() {
+        $('#logTable').DataTable();
+    });
+</script>
+<div class="main-container container">
+    <h1 class="log-h1">Eseménylista</h1>
+    <div class="log-table">
+        <table id="logTable" class="table table-striped table-bordered" >
+            <thead>
+                <tr>
+                    <th class="th-sm">id</th>
+                    <th class="th-sm">Action</th>
+                    <th class="th-sm">Value</th>
+                    <th class="th-sm">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php get_log(); ?>
+            </tbody>
+        </table>
+    </div>
+</div>
