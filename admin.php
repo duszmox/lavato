ca4c332981435bd82a20d1f135b7d156e6bc6198
Gyula Kiri <duszmo@f.fazekas.hu>
2020-03-01 16:11:01 +0100

    mid-day commit

--- admin.php
+++ admin.php
@@ -21,8 +21,9 @@
         <hr class="admin-hr">
         <ul class="admin-ul">
             <li class="admin-li"><a href="log.php" class="admin-li-a">Esemény lista</a></li>
-            <li class="admin-li"><a href="register.php" class="admin-li-a">Segítő Regisztrálása</a></li>
+            <li class="admin-li"><a href="register.php" class="admin-li-a">Segítő regisztrálása</a></li>
             <li class="admin-li"><a href="hashgen.php" class="admin-li-a">Kódok generálása</a></li>
+            <li class="admin-li"><a href="hashexport.php" class="admin-li-a">Kódok exportálása</a></li>
 
 
         </ul>
