556109f409081187b0434cf8164571c65163ea6d
Gyula Kiri <duszmo@f.fazekas.hu>
2020-03-05 21:10:56 +0100

    fix some vote.php bug

--- vote.php
+++ vote.php
@@ -9,26 +9,26 @@ display_errors();
 
 <head>
 
-    
+
     <title>Lavató - Szavazás</title>
 
-<link rel="stylesheet" href="main_style.css">
-<link rel="stylesheet" href="assets/fonts/leaves/stylesheet.css">
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="assets/css/bootstrap.css">
-<script src="https://kit.fontawesome.com/e8464711df.js" crossorigin="anonymous"></script>
-<script src="main.js"></script>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery-3.4.1.slim.min.js" crossorigin="anonymous"></script>
-<script src="assets/js/popper.min.js" crossorigin="anonymous"></script>
-<script src="assets/js/bootstrap.min.js" crossorigin="anonymous"></script>
-<script src="assets/js/core.js"></script>
-<script src="assets/js/charts.js"></script>
-<script src="assets/js/animated.js"></script>
-<script src="assets/js/sweetalert2@9.js"></script>
+    <link rel="stylesheet" href="main_style.css">
+    <link rel="stylesheet" href="assets/fonts/leaves/stylesheet.css">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <script src="https://kit.fontawesome.com/e8464711df.js" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery-3.4.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="assets/js/popper.min.js" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="assets/js/core.js"></script>
+    <script src="assets/js/charts.js"></script>
+    <script src="assets/js/animated.js"></script>
+    <script src="assets/js/sweetalert2@9.js"></script>
 
-    
+
 </head>
 
 <body class="bg">
@@ -44,19 +44,56 @@ display_errors();
 
         if (!verify_hash($hash)) {
             echo '<script> notRealCode(); </script>';
-    ?>
-            <script>
-                visibleForm();
-            </script>
-            <?php
-        } elseif (verify_hash($hash)) {
+            require_once("navbar.php");
 
-            if (!hasBeenActivated($hash)) {
-            ?><script>
+    ?>
+            <div class="main-container container">
+                <div class="code-padding">
+                    <h1 class="vote-h1">Szavazás</h1>
+                    <div class="code-box">
+                        <form id="code-form">
+                            <div class="form-group" id="form-group">
+                                <label for="exampleInputEmail1">Kód:</label>
+                                <input type="text" class="form-control" name="hash" id="hash" placeholder="Írd be a kódod" onkeyup="this.value = this.value.toLowerCase();">
+                                <small id="emailHelp" class="form-text text-muted">A Szavazó lapkán található kódot írd be ide, vagy
+                                    olvasd be a QR code-ot!
+                                </small>
+                            </div>
+                            <input type="submit" class="btn btn-primary" id="btn" value="Beküldés">
+                        </form>
+                    </div>
+                </div>
+
+
+            </div> <?php
+                } elseif (verify_hash($hash)) {
+
+                    if (!hasBeenActivated($hash)) {
+                    ?><script>
                     codeAlreadyActivated();
-                </script> <?php
-                        } else {
-                            ?>
+                </script>
+                <?php require_once("navbar.php") ?>
+                <div class="main-container container">
+                    <div class="code-padding">
+                        <h1 class="vote-h1">Szavazás</h1>
+                        <div class="code-box">
+                            <form id="code-form">
+                                <div class="form-group" id="form-group">
+                                    <label for="exampleInputEmail1">Kód:</label>
+                                    <input type="text" class="form-control" name="hash" id="hash" placeholder="Írd be a kódod" onkeyup="this.value = this.value.toLowerCase();">
+                                    <small id="emailHelp" class="form-text text-muted">A Szavazó lapkán található kódot írd be ide, vagy
+                                        olvasd be a QR code-ot!
+                                    </small>
+                                </div>
+                                <input type="submit" class="btn btn-primary" id="btn" value="Beküldés">
+                            </form>
+                        </div>
+                    </div>
+
+
+                </div><?php
+                    } else {
+                        ?>
                 <script type="text/javascript">
                     window.setTimeout(hideForm, 2);
                     voteInfo();
@@ -65,14 +102,14 @@ display_errors();
                     <div class="choose-class-A" onclick="confirmVote(this.textContent, true, '<?php echo $hash; ?>')"><p class="choose-class-text">A</p></div>
                     <div class="choose-class-B" onclick="confirmVote(this.textContent, false, '<?php echo $hash; ?>')"><p class="choose-class-text">B</p></div>
                     <div class="choose-class-C" onclick="confirmVote(this.textContent, false, '<?php echo $hash; ?>')"><p class="choose-class-text">C</p></div>
-                    <div class="choose-class-D" onclick="confirmVote(this.textContent, false, '<?php echo $hash;?>')"><p class="choose-class-text">D</p></div>
+                    <div class="choose-class-D" onclick="confirmVote(this.textContent, false, '<?php echo $hash; ?>')"><p class="choose-class-text">D</p></div>
                 </div>
         <?php
-                        }
                     }
-                } else {
+                }
+            } else {
 
-                    require_once("navbar.php");
+                require_once("navbar.php");
 
         ?>
         <div class="main-container container">
@@ -96,18 +133,18 @@ display_errors();
         </div>
 
         <?php
-                }
+            }
 
 
-                if (isset($_GET['vote'])) {
-                    if ($_GET['vote'] == 1) {
+            if (isset($_GET['vote'])) {
+                if ($_GET['vote'] == 1) {
         ?>
             <script>
                 successfulVote();
             </script>
     <?php
-                    }
                 }
+            }
     ?>
 
 
