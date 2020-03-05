2b9b9c842c8ac0a6746e7593429f549381bc86c5
Gyula Kiri <duszmo@f.fazekas.hu>
2020-03-05 19:46:22 +0100

    Final QR commit

--- qr_code/index.php
+++ qr_code/index.php
@@ -3,19 +3,23 @@
 
     display_errors();
     global $page_url; 
-    
     if (!is_admin()) {
         header("Location: ../");
     } else if(isset($_GET["url"])){
+        
         global $conn;
         $data = get_hashes_from_database("lavato_keys","hash", $conn);
+        $i = 1;
         foreach($data as $key => $value){
             $url = $page_url . "vote.php?hash=".$value;
             $qr_core_url = create_googlechart_from_url($url);
-            save_image_from_website($qr_core_url, "qr_codes/", "qr".date("Y-s-m").rand());
+            save_image_from_website($qr_core_url, "qr_codes/", "QR-Code-".$i);
+            $i++;
 
         }
-        download_folder_in_zip("qr_codes.zip");
+        download_folder_in_zip();
+        delete_files("qr_codes/");
+        unlink($zipFile);
     }
  
 ?>
--- snippets.php
+++ snippets.php
@@ -274,48 +274,65 @@ function displayErrors()
 }
 
 function create_googlechart_from_url($url)
-    {
-        return "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=".urlencode($url)."&choe=UTF-8";
-    }
-
-    function save_image_from_website($url, $dest, $name){
-        $img=file_get_contents($url);
-        file_put_contents($dest.substr(($name.".png"), strrpos($name,'/')), $img);
-
-    }
-    function download_folder_in_zip($download){ 
-        $zipFile = "test_dir.zip";
-        $zip = new ZipArchive;
-        if ($zip->open('test_dir.zip', ZipArchive::OVERWRITE) === TRUE)
-        {
-            if ($handle = opendir('qr_codes'))
-            {
-                while (false !== ($entry = readdir($handle)))
-                {
-                    if ($entry != "." && $entry != ".." && !is_dir('qr_codes/' . $entry))
-                    {
-                        $zip->addFile('qr_codes/' . $entry); 
-                    }
-                }
-                closedir($handle);
-            }
-            $zip->close();
-        }
-
-header('Content-Type: application/zip');
-header('Content-Disposition: attachment; filename='.basename($zipFile));
-readfile($zipFile);
-    
+{
+    return "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=" . urlencode($url) . "&choe=UTF-8";
 }
-    
-    function get_hashes_from_database($table, $column, $conn){
-        $sql = "SELECT $column FROM $table";
 
-        $data = array();
-        $result = mysqli_query($conn, $sql);
-        while($row = mysqli_fetch_assoc($result)){
-            $data[] = $row[$column];
+function save_image_from_website($url, $dest, $name)
+{   
+
+    
+    $img = file_get_contents($url);
+    file_put_contents($dest . substr(($name . ".png"), strrpos($name, '/')), $img);
+}
+function download_folder_in_zip()
+{
+    $zipFile = "codes.zip";
+    $zip = new ZipArchive;
+    if ($zip->open('codes.zip', ZipArchive::OVERWRITE) === TRUE) {
+        if ($handle = opendir('qr_codes')) {
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry != "." && $entry != ".." && !is_dir('qr_codes/' . $entry)) {
+                    $zip->addFile('qr_codes/' . $entry);
+                }
+            }
+            closedir($handle);
         }
-        return $data;
+        $zip->close();
     }
 
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename=' . basename($zipFile));
+    readfile($zipFile);
+    return $zipFile;
+}
+
+function get_hashes_from_database($table, $column, $conn)
+{
+    $sql = "SELECT $column FROM $table";
+
+    $data = array();
+    $result = mysqli_query($conn, $sql);
+    $num_rows = mysqli_num_rows($result);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row[$column];
+    }
+    return $data;
+}
+function delete_files($target) {
+    if(is_dir($target)){
+        $files = glob( $target . '*', GLOB_MARK );
+        foreach( $files as $file ){
+            delete_files( $file );      
+        }
+    } elseif(is_file($target)) {
+        unlink( $target );  
+    }
+}
+function get_hash_row_number() {
+    global $conn;
+    $sql = "SELECT * from lavato_keys";
+    $result = mysqli_query($conn, $sql);
+    $num_rows = mysqli_num_rows($result);
+    return $num_rows;
+}
