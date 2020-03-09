1c7033f15d5798772730fddba857b8b0bc7da64c
Gyula Kiri <duszmo@f.fazekas.hu>
2020-03-09 20:42:24 +0100

    Fixes

--- qr_code/index.php
+++ qr_code/index.php
@@ -20,11 +20,8 @@
  
         }
         header("Content-Type: text/html");  
-        
-        $zipFile = download_folder_in_zip("final_images");
+        $folder = "final_images";
+        $zipFile = download_folder_in_zip($folder);
         delete_files("final_images/");
-        unlink($zipFile);
-        log_action("Exported to cards", $_SESSION["username"]);
+        unlink("*.zip");
     }
- 
-?>
